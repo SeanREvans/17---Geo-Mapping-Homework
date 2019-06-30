beee8394f4769c01f46fc906050b9f8c1d813322
Sean Evans <seanrevans91@gmail.com>
2019-06-29 21:40:45 -0400

    grade attempt

--- Homework/logic.js
+++ Homework/logic.js
@@ -17,11 +17,25 @@ function createFeatures(earthquakeData) {
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Create a GeoJSON layer containing the features on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+    pointToLayer: function (feature, latlng) {
+              return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: fillColor(feature.properties.mag),
+        color: "black",
+        weight: 0.6,
+        opacity: 0.4,
+        fillOpacity: 0.6
+      });
+      },
+
+      // Create popups
+      onEachFeature: function (feature, layer) {
+        return layer.bindPopup(`<strong>Place:</strong> ${feature.properties.place}<br><strong>Magnitude:</strong> ${feature.properties.mag}`);
+      }
+    });
 
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
@@ -70,4 +84,67 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+
+// Set up the legend and the colour variance for magnitude from leaflet documentation
+var legend = L.control({ position: 'bottomright'});
+
+
+  legend.onAdd = function() {
+    var div = L.DomUtil.create('div', 'info legend'),
+        magnitude = [0,1,2,3,4,5,6],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < magnitude.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + fillColor(magnitude[i] + 1) + '"></i> ' +
+            magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+            console.log(div.innerHTML)
+          }
+
+    return div;
+};
+
+legend.addTo(myMap);
+  };
+   // Adding legend to the map
+
+// Define colors depending on the magnituge of the earthquake
+function fillColor(magnituge) {
+
+    switch (true) {
+      case magnituge >= 6.0:
+        return 'red';
+        break;
+      
+      case magnituge >= 5.0:
+        return 'orangered';
+        break;
+
+      case magnituge >= 4.0:
+        return 'darkorange';
+        break;
+      
+      case magnituge >= 3.0:
+        return 'orange';
+        break;
+
+      case magnituge >= 2.0:
+        return 'gold';
+        break;
+
+      case magnituge >= 1.0:
+        return 'yellow';
+        break;
+
+      default:
+        return 'greenyellow';
+    };
+};
+
+
+// Reflect the earthquake magnitude
+function markerSize(magnituge) {
+  return magnituge*3;
 }
